430fc3aa7ebc31882c6af724577c8446716d8f5c
stevic.nenad@icloud.com <NStevic@hotmail.com>
2023-06-24 01:07:09 +0200

    implementing color picker, afterwards tackling saving of data to firebase

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,15 +1,24 @@
 import { Box, TextField, Button, Container, Typography } from "@mui/material";
 import { useState } from "react";
+import { nanoid } from "nanoid";
+import Cookies from "js-cookie";
 
 const LandingPage = () => {
   const [code, setCode] = useState("");
+  const [showColorSelection, setShowColorSelection] = useState<Boolean>(false);
 
   const handleJoin = () => {
     // handle joining the game with the code
   };
 
   const handleCreate = () => {
-    // handle creating a new game
+    let userID = Cookies.get("userID"); // Check if the userID cookie exists
+    if (!userID) {
+      userID = nanoid(); // Generate a new user ID
+      Cookies.set("userID", userID); // Save the user ID to a cookie
+    }
+    const lobbyCode = nanoid(8);
+    setShowColorSelection(true);
   };
 
   return (
@@ -19,7 +28,7 @@ const LandingPage = () => {
         overflow: "hidden",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
       }}
     >
       <video
@@ -52,7 +61,7 @@ const LandingPage = () => {
           alignItems: "center",
           justifyContent: "center",
           height: "100vh",
-          zIndex: "1"
+          zIndex: "1",
         }}
       >
         <Box
@@ -63,11 +72,14 @@ const LandingPage = () => {
             alignItems: "center",
             gap: "3ch",
             width: "45ch",
-            p: 3, // padding
+            p: 3,
             bgcolor: "rgba(255, 255, 255, 0.1)",
             backdropFilter: "blur(8px)",
             zIndex: "1",
             borderRadius: "1ch",
+            transition: "opacity 0.3s",
+            opacity: showColorSelection ? 0 : 1,
+            visiblity: showColorSelection ? "hidden" : "visible",
           }}
           noValidate
           autoComplete="off"
@@ -75,13 +87,13 @@ const LandingPage = () => {
           <img
             src={"/logo.svg"}
             alt="Logo"
-            style={{ width: "80%", padding: "3ch" }}
+            style={{ width: "80%", padding: "6ch" }}
           />
           <Box
             sx={{ display: "flex", gap: "10px", zIndex: "1", width: "100%" }}
           >
             <TextField
-              label="Enter Game Code"
+              label="Enter Lobby Code"
               variant="outlined"
               value={code}
               fullWidth
@@ -94,6 +106,76 @@ const LandingPage = () => {
           <Button variant="text" onClick={handleCreate}>
             Create Lobby
           </Button>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            zIndex: "1",
+            borderRadius: "1ch",
+            transition: "opacity 0.5s 0.5s",
+            opacity: showColorSelection ? 1 : 0,
+            visiblity: showColorSelection ? "visible" : "hidden",
+          }}
+        >
+          <img
+            src={"/choose-color.svg"}
+            alt="choose color"
+            style={{ width: "80%", padding: "6ch" }}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              zIndex: "1",
+              borderRadius: "1ch",
+            }}
+          >
+            <Box
+              component="form"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "3ch",
+                width: "45ch",
+                p: 3,
+                bgcolor: "rgba(255, 255, 255, 0.1)",
+                backdropFilter: "blur(8px)",
+                zIndex: "1",
+                borderRadius: "1ch",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <Button variant="contained" onClick={handleJoin}>
+                Join
+              </Button>
+            </Box>
+            <Box
+              component="form"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "3ch",
+                width: "45ch",
+                p: 3,
+                bgcolor: "rgba(255, 255, 255, 0.1)",
+                backdropFilter: "blur(8px)",
+                zIndex: "1",
+                borderRadius: "1ch",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <Button variant="contained" onClick={handleJoin}>
+                Join
+              </Button>
+            </Box>
+          </Box>
         </Box>
       </Container>
     </div>
